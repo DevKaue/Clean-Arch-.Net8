e1c4af1949787b7cbadcdc26d5cbb3309713ac60
Kaue <kauesabino@hotmail.com>
2025-06-05 01:10:27 -0300

    feat: Criando as Entidades User e WorkSpace

--- TasksApp/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ TasksApp/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e718483fea25910bb0f0f05b1e5f5ae856b02dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
