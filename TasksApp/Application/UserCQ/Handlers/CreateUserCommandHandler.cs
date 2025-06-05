e0a626658b602c2af8f5c743349de90644ba6641
Kaue <kauesabino@hotmail.com>
2025-06-05 15:41:34 -0300

    feat: fazendo montagem do fluxo basico do CQRS para o User

---
+++ TasksApp/Application/UserCQ/Handlers/CreateUserCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.UserCQ.Commands;
+using Application.UserCQ.ViewModels;
+using MediatR;
+
+namespace Application.UserCQ.Handlers
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserInfoViewModel>
+    {
+        public Task<UserInfoViewModel> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TasksApp/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ TasksApp/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5d67279fa2a98a1f825a2a170946fee46de012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f09a9e147e14acd0062da8f9f2b82afad890fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
